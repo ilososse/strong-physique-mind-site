0c41ed3cb841e8e611c661134e8210caac51d4bc
Ilyas Moudden <ilyas.moud@gmail.com>
2025-12-30 16:34:13 +0100

    Improving app menus display

--- counter/assets/launcher-BfOdzBBC.js
+++ counter/assets/launcher-BfOdzBBC.js
@@ -3781,8 +3781,8 @@ void main() {
       cursor: pointer;
     `,Object.keys(this.EXERCISES).forEach(o=>{const a=document.createElement("option");a.value=o,a.textContent=o,n.appendChild(a)}),n.value=((r=this.exercise)==null?void 0:r.name)||"Tractions",n.addEventListener("change",()=>{var a;this._setExercise(n.value);const o=(a=this.liveMenuContainer)==null?void 0:a.querySelector("select");o&&(o.value=n.value)}),e.appendChild(t),e.appendChild(n),(s=this.startButton)!=null&&s.parentElement?this.startButton.parentElement.insertBefore(e,this.startButton):this.startScreenOverlay.appendChild(e),this.exerciseSelect=n}_injectExerciseMenuLive(){var o;if(!this.renderDiv)return;const e=document.createElement("div");e.style.cssText=`
       position: absolute;
-      top: 80px;
-      left: 70%;
+      top: 60px;
+      right: 12px;
       transform: translateX(-50%);
       z-index: 999999;
       display: none; /* affiché à Start */
