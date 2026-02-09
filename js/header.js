4f95c3f739d694c863793c1e4d5f3ed4f02807d3
Ilyas Moudden <ilyas.moud@gmail.com>
2026-02-10 00:35:38 +0100

    Improving navigation through Toggle menu

--- js/header.js
+++ js/header.js
@@ -5,5 +5,25 @@ const SUPABASE_ANON_KEY = "sb_publishable_8cLzA_3Ud6AaLSi8mTf1Gw_4lJZTLjs";
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 function toggleMenu() {
-  document.getElementById("mobileNav").classList.toggle("open");
+  const menu = document.getElementById("mobileNav");
+  menu.classList.toggle("open");
 }
+
+function closeMenu() {
+  const menu = document.getElementById("mobileNav");
+  menu.classList.remove("open");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileNav = document.getElementById("mobileNav");
+
+  // Si la page n’a pas de menu mobile (sécurité)
+  if (!mobileNav) return;
+
+  mobileNav.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+      closeMenu();
+    });
+  });
+});
+
